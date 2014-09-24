7b3bf0a0e327df740d0d8bed8cf161acf81562b9
WalterPeck <chrislabarge2@gmail.com>
2014-09-23 21:57:11 -0400

    i forgot suttin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ SjManagement::Application.configure do
 	 	:authentication => :plain,
 	 	:user_name			=> ENV['SENDGRID_USERNAME'],
 		:password				=> ENV['SENDGRID_PASSWORD'],
-		:domain					=> 'heroku.com'
+		:domain					=> 'heroku.com',
 		:enable_starttls_auto => true
 	}
 
