0b93cc69b52c48ca8c2c453c1de6115f19d7f217
WalterPeck <chrislabarge2@gmail.com>
2014-09-25 22:11:11 -0400

    I got the profession variable to work for the training contact form which is prettty awesome.

--- app/controllers/training_controller.rb
+++ app/controllers/training_controller.rb
@@ -11,7 +11,8 @@ class TrainingController < ApplicationController
 		name = params[:name]
 		email = params[:email]
 		body = params[:comments]
-		UserMailer.training_email('clabvessels@gmail.com',name,email,body).deliver
+		profession = params[:profession]
+		UserMailer.training_email('clabvessels@gmail.com',name,email,profession,body).deliver
 		redirect_to root_path
 	end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,12 @@
 class UserMailer < ActionMailer::Base
   default from: "chrislabarge2@gmail.com"
 	
-	def training_email(company_email, name, email, body)
+	def training_email(company_email, name, email, profession, body)
 		@name = name 
 		@email = email
+		@profession = profession
 		@body = body
-		
+				
 		mail(to: company_email, subject: "NEW Training Client!!" )
 	end
 	
