9dc87e7c8c012448f4b877d7b033ed27f48d267c
WalterPeck <chrislabarge2@gmail.com>
2014-10-05 19:56:24 -0400

    training page layout somemore...just fuggin around.  Also fixed the modal header problem for the training form.. messed with some typography as well.  still need to go back and make the <p> font responsive.. right now I'm trying to figure out why I cannot edit the email Jeff will get when someone signs up.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
   default from: "chrislabarge2@gmail.com"
 	
-	def training_email(company_email, name, email, company, body, city, state)
+	def training_email(sj_email, name, email, company, body, city, state)
 		@name = name 
 		@email = email
 		@company = company
@@ -9,7 +9,7 @@ class UserMailer < ActionMailer::Base
 		@city = city
 		@state = state
 				
-		mail(to: company_email, subject: "NEW Training Client!!" )
+		mail(to: sj_email, subject: "NEW Training Client!!" )
 	end
 	
 end
